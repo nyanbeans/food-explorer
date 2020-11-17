f0d96796d6b4b8d5327f2372d571aeb4f8a7fcb7
Matt Hildebrand <hildebrand.matt@gmail.com>
2020-11-16 16:08:45 -0800

    Updated nav button toggle

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,10 @@
 // Write your JavaScript here...
+
+//Script for Nav Button//
+
+const body = document.body;
+const btn = document.querySelector('.btn-menu');
+
+btn.addEventListener('click', function(){
+    body.classList.toggle('show');
+});
